74ee854b54f3b5251d391a11957ad6e432d293ba
Aleks Rutins <keeper@farthergate.com>
2026-01-12 08:12:45 -0500

    Better styling

--- main.go
+++ main.go
@@ -6,20 +6,27 @@ import (
 	"os/exec"
 
 	"farthergate.com/goldberg/config"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 )
 
 func main() {
 	cfg, err := config.LoadFromPath(context.Background(), "goldberg.pkl")
 
+	log.SetStyles(log.DefaultStyles())
+	log.SetReportTimestamp(false)
+
+	stepStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("35"))
+	cmnd := lipgloss.NewStyle().Foreground(lipgloss.Color("31"))
+
 	if err != nil {
 		panic(err)
 	}
 
 	for _, step := range cfg.Steps {
-		log.Print("STEP", "name", step.Name)
+		log.Print(stepStyle.Render("STEP"), "name", step.Name)
 		for _, command := range step.Commands {
-			log.Print("CMND", "cmd", command)
+			log.Print(cmnd.Render("CMND"), "cmd", command)
 			cmd := exec.Command(command[0], command[1:]...)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
